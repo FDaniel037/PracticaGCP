1bec8d0fce4102228be620bde5c908bc74a13c8a
FDaniel037 <danielito037g@gmail.com>
2024-11-06 11:38:08 -0500

    Update deportes.php

--- api/secciones/deportes.php
+++ api/secciones/deportes.php
@@ -6,6 +6,6 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 $deportes = [
 "titulo" => "Una mirada a Perú desde Chile: qué piensan de la Bicolor y por qué será un partido de vida o muerte",
 "autor" => "Roy Galdos Sánchez",
-"resumen" => "A poco más de una semana para el partido en Lima, Depor conversó con dos periodistas chilenos para saber qué piensan de la Blanquirroja en el país sureño. Christopher Brandt, por su parte, reveló que la ANFP le puso a Gareca un mínimo de cuatro puntos en esta fecha doble. “Lo ideal es conseguir los seis puntos. Chile está muy complicado en la tabla de posiciones, necesita acercarse y va a jugar contra rivales directos como Perú y Venezuela. Hay un piso de cuatro puntos que le pidió la ANFP a Gareca, lo que habla de un empate en Lima y un triunfo en Santiago. La expectativa no es de las mejores, pero si Chile logra esa cantidad de puntos probablemente Gareca siga con el proceso y haya un poco más de esperanza pensando en lo que va a hacer luego”",
+"resumen" => "Christopher Brandt, reveló que la ANFP le puso a Gareca un mínimo de cuatro puntos en esta fecha doble. “Lo ideal es conseguir los seis puntos. Chile está muy complicado en la tabla de posiciones, necesita acercarse y va a jugar contra rivales directos como Perú y Venezuela. La expectativa no es de las mejores.",
 ];
 ?>
