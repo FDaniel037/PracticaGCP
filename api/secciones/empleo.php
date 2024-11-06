8ea2d5e7f0e6dbebb13bb275089103d21928be04
FDaniel037 <danielito037g@gmail.com>
2024-11-06 11:41:09 -0500

    Update empleo.php

--- api/secciones/empleo.php
+++ api/secciones/empleo.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $empleo = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "La época del año con mayor rotación laboral en puestos junior y senior",
+"autor" => "Mía Ríos",
+"resumen" => "La rotación laboral es una de las preocupaciones más grandes de los empleadores, ya que parte de lo que se invierte en el proceso de reclutamiento, evaluaciones, horas de capacitación, entre otros, se termina perdiendo debido a la pronta salida de un grupo de trabajadores.",
 ];
 ?>
