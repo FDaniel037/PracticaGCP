a3d2cde406b0df54f2a597ac3c768dd55c1bf6de
FDaniel037 <danielito037g@gmail.com>
2024-11-06 11:52:40 -0500

    Update openshift.php

--- api/secciones/openshift.php
+++ api/secciones/openshift.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $openshift = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "¿Cómo alcanzar la innovación a través de los servicios de nube de Red Hat OpenShift?",
+"autor" => "Lener Barrios",
+"resumen" => "Invito a todo el ecosistema de socios de Perú a acercarse a LOL para conocer más sobre las bondades de la tecnología open source de Red Hat. Contamos con un equipo de ingenieros técnicos calificados en toda la región para extender la colaboración de nuestros socios a sus clientes finales, como así también apoyo financiero y de marketing",
 ];
 ?>
