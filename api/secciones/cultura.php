d9aaeb34df889c4ea8aa12c0869f3f0e30541116
FDaniel037 <danielito037g@gmail.com>
2024-11-06 11:37:11 -0500

    Update cultura.php

--- api/secciones/cultura.php
+++ api/secciones/cultura.php
@@ -6,6 +6,6 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 $cultura = [
 "titulo" => "Aventura en Galápagos: viajamos a estas islas para conocer su fascinante vida marina, reptiles terrestres e islas volcánicas",
 "autor" => "Nora Sugobono",
-"resumen" => "Es uno de los parajes más fantásticos del mundo. Hogar de reptiles, mamíferos, aves y peces que conviven en perfecto equilibrio con los seres humanos. A tan solo unas horas del Perú, las islas Galápagos son un destino idóneo para los amantes de la naturaleza. En el camino, nos dejan una inmensa lección de cómo el turismo sostenible es la clave para la evolución.",
+"resumen" => "Es uno de los parajes más fantásticos del mundo. Hogar de reptiles, mamíferos, aves y peces que conviven en perfecto equilibrio con los seres humanos. A tan solo unas horas del Perú, las islas Galápagos son un destino idóneo para los amantes de la naturaleza.",
 ];
 ?>
