7aaf0cbe731826ad112b9ca8b422f7296504a8a7
FDaniel037 <danielito037g@gmail.com>
2024-11-06 12:00:56 -0500

    Update video.php

--- api/secciones/video.php
+++ api/secciones/video.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $video = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Un votante de un estado indeciso dice que su novia influyó en su voto",
+"autor" => "CNN",
+"resumen" => "Brianna Keilar de CNN entrevista a un votante de Carolina del Norte que dice que no iba a votar hasta que su novia intervino.",
 ];
 ?>
